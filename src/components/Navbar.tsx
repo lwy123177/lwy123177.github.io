bdc630912bf240d686dd5c64b18b13a75285cd02
waiyip <lwy123177@gmail.com>
2022-11-12 21:43:04 +0800

    Import home icon and improve style

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { AppBar, Toolbar, Button } from "@mui/material";
 import { Link, useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
+import HomeIcon from "@mui/icons-material/Home";
 
 const Navbar = () => {
   const location = useLocation();
@@ -12,15 +13,15 @@ const Navbar = () => {
   return (
     <AppBar position="static" color="secondary">
       <Toolbar variant="dense">
-        <Button variant={path == "/" ? "outlined" : "text"}>
-          <Link to="/">Home</Link>
-        </Button>
-        <Button variant={path == "/bfs" ? "outlined" : "text"}>
-          <Link to="/bfs">BFS</Link>
-        </Button>
-        <Button variant={path == "/dfs" ? "outlined" : "text"}>
-          <Link to="/dfs">DFS</Link>
-        </Button>
+        <Link to="/">
+          <HomeIcon color={path == "/" ? "warning" : "primary"} />
+        </Link>
+        <Link to="/bfs">
+          <Button color={path == "/bfs" ? "warning" : "primary"}>BFS</Button>
+        </Link>
+        <Link to="/dfs">
+          <Button color={path == "/dfs" ? "warning" : "primary"}>DFS</Button>
+        </Link>
       </Toolbar>
     </AppBar>
   );
