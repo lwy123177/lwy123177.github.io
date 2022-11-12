6935141d06795bfef6eae792a9e9bbd07f9053ea
waiyip <lwy123177@gmail.com>
2022-11-12 21:19:56 +0800

    Update nav bar to highlight current page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,24 @@
 import { AppBar, Toolbar, Button } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
+  const location = useLocation();
+  const [path, setPath] = useState<string>();
+  useEffect(() => {
+    setPath(location.pathname);
+  }, [location]);
+
   return (
     <AppBar position="static" color="secondary">
       <Toolbar variant="dense">
-        <Button color="primary">
+        <Button variant={path == "/" ? "outlined" : "text"}>
           <Link to="/">Home</Link>
         </Button>
-        <Button color="primary">
+        <Button variant={path == "/bfs" ? "outlined" : "text"}>
           <Link to="/bfs">BFS</Link>
         </Button>
-        <Button color="primary">
+        <Button variant={path == "/dfs" ? "outlined" : "text"}>
           <Link to="/dfs">DFS</Link>
         </Button>
       </Toolbar>
