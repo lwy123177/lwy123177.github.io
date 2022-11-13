20809ffdefdbd24c27850a51fa0512710f738e40
waiyip <lwy123177@gmail.com>
2022-11-13 19:43:45 +0800

    Update state for the program

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,6 +1,7 @@
 import classes from "./Grid.module.css";
 import { useContext } from "react";
 import { GridContext } from "../store/GridContext";
+import { Button } from "@mui/material";
 
 const Label: React.FC<{ name: string }> = (props) => {
   return (
@@ -19,6 +20,26 @@ const Grid: React.FC<{
   grid: string[][];
 }> = (props) => {
   const gridContext = useContext(GridContext);
+  const drawPathHandler = () => {
+    // TODO: use some algorithm (bfs/dfs) to find the path here
+    gridContext.nextTurn();
+  };
+  const getInstruction = (state: string) => {
+    if (state === "Start") return <h3>Please select a starting position</h3>;
+    if (state === "Destination") return <h3>Please select a destination</h3>;
+    if (state === "Obstacle") return <h3>Please select obstacle(s)"</h3>;
+    if (state === "ReadyToDraw")
+      return (
+        <Button variant="outlined" onClick={drawPathHandler}>
+          Draw the path!
+        </Button>
+      );
+    if (state === "Drawing") return <h3>Finding the path...</h3>;
+  };
+  const cellClickHandler = (row: number, col: number) => {
+    gridContext.markCell(row, col, gridContext.state.toLowerCase());
+    gridContext.nextTurn();
+  };
   return (
     <>
       <div className={classes["label-row"]}>
@@ -27,15 +48,14 @@ const Grid: React.FC<{
         <Label name={"Current"} />
         <Label name={"Destination"} />
       </div>
+      {getInstruction(gridContext.state)}
       <div className={classes["div-table"]}>
         {props.grid.map((row, rowIndex) => (
           <div className={classes["div-table-row"]}>
             {row.map((cell, colIndex) => (
               <div
                 className={`${classes["div-table-col"]} ${classes[cell]}`}
-                onClick={() => {
-                  gridContext.markCell(rowIndex, colIndex, "obstacle");
-                }}
+                onClick={() => cellClickHandler(rowIndex, colIndex)}
               />
             ))}
           </div>
--- src/store/GridContext.tsx
+++ src/store/GridContext.tsx
@@ -3,9 +3,19 @@ import React, { useState } from "react";
 const h = 40;
 const w = 50;
 
+type State =
+  | "Start"
+  | "Destination"
+  | "Obstacle"
+  | "ReadyToDraw"
+  | "Drawing"
+  | "FinishedDrawing";
+
 type GridContextObj = {
   grid: string[][];
   markCell: (row: number, col: number, val: string) => void;
+  state: State;
+  nextTurn: () => void;
 };
 
 let startGrid: string[][] = [];
@@ -20,12 +30,15 @@ for (let i = 0; i < h; i++) {
 export const GridContext = React.createContext<GridContextObj>({
   grid: startGrid,
   markCell: (row: number, col: number, val: string) => {},
+  state: "Start",
+  nextTurn: () => {},
 });
 
 export const GridContextProvider: React.FC<{ children: React.ReactNode }> = (
   props
 ) => {
   const [grid, setGrid] = useState<string[][]>(startGrid);
+  const [state, setSelecting] = useState<State>("Start");
   const markCell = (row: number, col: number, val: string) => {
     setGrid((prevState) => {
       let cloneGrid: string[][] = [];
@@ -36,9 +49,22 @@ export const GridContextProvider: React.FC<{ children: React.ReactNode }> = (
       return cloneGrid;
     });
   };
+  const nextTurn = () => {
+    setSelecting((prevState) => {
+      if (prevState === "Start") return "Destination";
+      if (prevState === "Destination") return "Obstacle";
+      if (prevState === "Obstacle") return "ReadyToDraw";
+      if (prevState === "ReadyToDraw") return "Drawing";
+      if (prevState === "Drawing") return "FinishedDrawing";
+      // if (prevState === "FinishedDrawing")
+      return "Start";
+    });
+  };
   const contextValue = {
     grid: grid,
     markCell: markCell,
+    state: state,
+    nextTurn: nextTurn,
   };
   return (
     <GridContext.Provider value={contextValue}>
