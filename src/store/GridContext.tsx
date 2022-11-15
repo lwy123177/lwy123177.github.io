0b1247c83bc814411a90b0f4bedc6de01e0d7705
waiyip <lwy123177@gmail.com>
2022-11-15 17:52:20 +0800

    Refactor to useCallback() to fix useEffect missing dependency warning

--- src/store/GridContext.tsx
+++ src/store/GridContext.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 const h = 30;
 const w = 30;
@@ -41,35 +41,41 @@ export const GridContextProvider: React.FC<{ children: React.ReactNode }> = (
   const [grid, setGrid] = useState<string[][]>(startGrid);
   const [state, setState] = useState<State>("Start");
   const [obstacles, setObstacles] = useState<[number, number][]>([]);
-  const out = (row: number, col: number) => {
-    return row < 0 || col < 0 || row >= grid.length || col >= grid[0].length;
-  };
-  const markCell = (row: number, col: number, val: string) => {
-    if (
-      out(row, col) ||
-      (grid[row][col] !== "obstacle" && grid[row][col] !== "empty") ||
-      (grid[row][col] === "obstacle" && val !== "empty")
-    )
-      return false;
-    setGrid((prevState) => {
-      let cloneGrid: string[][] = [];
-      for (let i = 0; i < prevState.length; i++) {
-        cloneGrid.push([...prevState[i]]);
+  const out = useCallback(
+    (row: number, col: number) => {
+      return row < 0 || col < 0 || row >= grid.length || col >= grid[0].length;
+    },
+    [grid]
+  );
+  const markCell = useCallback(
+    (row: number, col: number, val: string) => {
+      if (
+        out(row, col) ||
+        (grid[row][col] !== "obstacle" && grid[row][col] !== "empty") ||
+        (grid[row][col] === "obstacle" && val !== "empty")
+      )
+        return false;
+      setGrid((prevState) => {
+        let cloneGrid: string[][] = [];
+        for (let i = 0; i < prevState.length; i++) {
+          cloneGrid.push([...prevState[i]]);
+        }
+        cloneGrid[row][col] = val;
+        return cloneGrid;
+      });
+      if (val === "obstacle") {
+        setObstacles((prevState) => [...prevState, [row, col]]);
       }
-      cloneGrid[row][col] = val;
-      return cloneGrid;
-    });
-    if (val === "obstacle") {
-      setObstacles((prevState) => [...prevState, [row, col]]);
-    }
-    return true;
-  };
-  const undo = () => {
+      return true;
+    },
+    [grid, out]
+  );
+  const undo = useCallback(() => {
     if (state !== "Obstacle" || obstacles.length === 0) return;
     let popped = obstacles[obstacles.length - 1]!;
     markCell(popped[0], popped[1], "empty");
     setObstacles((prevState) => prevState.slice(0, -1));
-  };
+  }, [markCell, obstacles, state]);
   const contextValue = {
     grid: grid,
     markCell: markCell,
@@ -77,19 +83,22 @@ export const GridContextProvider: React.FC<{ children: React.ReactNode }> = (
     setState: setState,
     out: out,
   };
-  const keyDownHandler = (event: KeyboardEvent) => {
-    const { key } = event;
-    let isCtrlKey = event.ctrlKey || event.metaKey;
-    let isZ = key.toLowerCase() === "z";
-    if (isCtrlKey && isZ) {
-      undo();
-      event.preventDefault();
-    }
-  };
+  const keyDownHandler = useCallback(
+    (event: KeyboardEvent) => {
+      const { key } = event;
+      let isCtrlKey = event.ctrlKey || event.metaKey;
+      let isZ = key.toLowerCase() === "z";
+      if (isCtrlKey && isZ) {
+        undo();
+        event.preventDefault();
+      }
+    },
+    [undo]
+  );
   useEffect(() => {
     window.addEventListener("keydown", keyDownHandler);
     return () => window.removeEventListener("keydown", keyDownHandler);
-  }, [obstacles, state]);
+  }, [obstacles, state, keyDownHandler]);
 
   return (
     <GridContext.Provider value={contextValue}>
