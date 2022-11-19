4346f3a89a7940c1fa4ae8d0a55674fa6e610ca3
waiyip <lwy123177@gmail.com>
2022-11-19 13:30:01 +0800

    Update styling and font size

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -43,27 +43,27 @@ const Grid: React.FC<{
     gridContext.setState("Start");
   };
   const getInstruction = (state: State) => {
-    if (state === "Start") return <h3>Please select a starting position</h3>;
-    if (state === "Destination") return <h3>Please select a destination</h3>;
+    if (state === "Start") return <h5>Please select a starting position</h5>;
+    if (state === "Destination") return <h5>Please select a destination</h5>;
     if (state === "Obstacle")
       return (
         <div className={classes["label-row"]}>
-          <h3>
+          <h5>
             Please select obstacle(s){" "}
             <label className={classes.hint}>(Ctrl-Z to undo)</label>
-          </h3>
+          </h5>
           <Button variant="outlined" onClick={props.search}>
             Draw!
           </Button>
         </div>
       );
     if (state === "Drawing") {
-      return <h3>Finding the path...</h3>;
+      return <h5>Finding the path...</h5>;
     }
     if (state === "FinishedDrawing")
       return (
         <div className={classes["label-row"]}>
-          <h3>Done: </h3>
+          <h5>Done:</h5>
           <Button variant="outlined" onClick={handleDrawAgain}>
             Draw Again
           </Button>
